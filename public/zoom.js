80bd2d10bce2df2da62da89a52b324b0d7bcfd75
Gijs van Tulder <g.vantulder@erasmusmc.nl>
2018-11-20 11:50:08 +0100

    Remember zoom setting in a cookie.

--- public/zoom.js
+++ public/zoom.js
@@ -1,28 +1,63 @@
 
+// cookie helper functions from
+// https://www.quirksmode.org/js/cookies.html
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+
 // zoom buttons
 document.addEventListener('click', function(e) {
+  var zoomFactor = null;
   if (e.target && e.target.id == 'zoom-plus-button') {
-    var imgs = document.getElementById('task-images');
-    if (!imgs.style.zoom) {
-      imgs.style.zoom = 1.1;
-    } else {
-      imgs.style.zoom *= 1.1;
-    }
-    if (e.preventDefault) e.preventDefault();
-    if (e.stopPropagation) e.stopPropagation();
-    return false;
+    zoomFactor = 1.1;
   } else if (e.target && e.target.id == 'zoom-minus-button') {
+    zoomFactor = 1.0 / 1.1;
+  }
+  if (zoomFactor) {
     var imgs = document.getElementById('task-images');
-    if (!imgs.style.zoom) {
-      imgs.style.zoom = 1.0 / 1.1;
+    var newZoom = zoomFactor * (imgs.style.zoom ? imgs.style.zoom : 1);
+    if (Math.abs(newZoom - 1) < 1e-4) {
+      imgs.style.zoom = '';
+      eraseCookie('zoom-factor-' + document.body.getAttribute('data-task-set-id'));
     } else {
-      imgs.style.zoom *= 1.0 / 1.1;
+      imgs.style.zoom = newZoom;
+      createCookie('zoom-factor-' + document.body.getAttribute('data-task-set-id'), newZoom);
     }
     if (e.preventDefault) e.preventDefault();
     if (e.stopPropagation) e.stopPropagation();
     return false;
   }
 });
+
+(function() {
+  var zoomFactor = readCookie('zoom-factor-' + document.body.getAttribute('data-task-set-id'));
+  if (zoomFactor) {
+    var imgs = document.getElementById('task-images');
+    if (imgs) {
+      imgs.style.zoom = zoomFactor;
+    }
+  }
+})();
 
 // create a list of keyboard shortcuts
 var decisionsWithKeys = {};
